d15c4ed6129127a853d7c84590c9544f2a69e9a8
Matthias M. Fischer <33027321+Ma-Fi-94@users.noreply.github.com>
2023-01-16 15:08:11 +0100

    Finished 1-10

--- euler0001ff.hs
+++ euler0001ff.hs
@@ -49,6 +49,14 @@ problem6 n = sqSum - sumSq
     sqSum = (n * (n+1) `div` 2) ^ 2
     sumSq = n * (n+1) * (2*n+1) `div` 6
 
+-- Featuring the infamous prime sieve à la literateprograms.org. 
+-- This is nice code, but pretty slow due to inefficient filtering.
+problem7' :: Int -> Int
+problem7' n = primes!!(n-1)
+  where
+    primes = sieve [2..]
+    sieve (p:xs) = p : sieve [x | x<-xs, x `rem` p /= 0]
+
 -- A better implementation with efficient primeness check only up to sqrt(x).
 problem7 :: Int -> Int
 problem7 n = primes!!(n-1)
@@ -61,22 +69,55 @@ problem7 n = primes!!(n-1)
             |x `rem` d == 0 = False
             |otherwise      = go (d+1)
 
--- Featuring the infamous prime sieve à la literateprograms.org. 
--- This is nice code, but pretty slow due to inefficient filtering.
-problem7' :: Int -> Int
-problem7' n = primes!!(n-1)
+problem8 :: Integer -> Integer
+problem8 = maximum . map digProd . blocks
   where
-    primes = sieve [2..]
-    sieve (p:xs) = p : sieve [x | x<-xs, x `rem` p /= 0]
+    readInt = (read :: String -> Integer)
+    digProd = foldl1 (*) . map readInt . map (:[]) . show
+    blocks = go . show
+      where    
+        go x = case length x of
+            13 -> [readInt x]
+            _  -> readInt (take 13 x) : go (drop 1 x) 
 
+-- Efficient search strategy using the fact we only care about squares a^2,b^2,
+-- and generating a single candidate c^2 for every pair a^2, b^2
+problem9 :: Int -> Int
+problem9 s = head [(a*b*c) | a <- [1..s],
+                             b <- [a..s],
+                             let (c2,c) = (a^2 + b^2, isqrt(c2)),
+                             isSquare c2,
+                             b < c,
+                             a+b+c == s]
+  where
+    isqrt      = ceiling . sqrt . fromIntegral
+    isSquare n = n == isqrt(n)^2 
+
+-- Using the prime generation from problem7, which here is still acceptable,
+-- tho far from efficient.
+problem10 :: Int -> Int
+problem10 n = sum . takeWhile (<n) $ primes
+  where
+    primes = filter isPrime [2..]
+    isPrime x = go 2
+      where
+        go d
+            |d^2 > x        = True
+            |x `rem` d == 0 = False
+            |otherwise      = go (d+1)
+
+  
 main :: IO ()
 main = do
-    putStrLn $ "Problem 1: " ++ show (problem1 999)
-    putStrLn $ "Problem 2: " ++ show (problem2 4000000)
-    putStrLn $ "Problem 3: " ++ show (problem3 600851475143)
-    putStrLn $ "Problem 4: " ++ show (problem4)
-    putStrLn $ "Problem 5: " ++ show (problem5)
-    putStrLn $ "Problem 6: " ++ show (problem6 100)
-    putStrLn $ "Problem 7: " ++ show (problem7 10001)
+    -- putStrLn $ "Problem 1: " ++ show (problem1 999)
+    -- putStrLn $ "Problem 2: " ++ show (problem2 4000000)
+    -- putStrLn $ "Problem 3: " ++ show (problem3 600851475143)
+    -- putStrLn $ "Problem 4: " ++ show (problem4)
+    -- putStrLn $ "Problem 5: " ++ show (problem5)
+    -- putStrLn $ "Problem 6: " ++ show (problem6 100)
+    -- putStrLn $ "Problem 7: " ++ show (problem7 10001)
+    -- putStrLn $ "Problem 8: " ++ show (problem8 7316717653133062491922511967442657474235534919493496983520312774506326239578318016984801869478851843858615607891129494954595017379583319528532088055111254069874715852386305071569329096329522744304355766896648950445244523161731856403098711121722383113622298934233803081353362766142828064444866452387493035890729629049156044077239071381051585930796086670172427121883998797908792274921901699720888093776657273330010533678812202354218097512545405947522435258490771167055601360483958644670632441572215539753697817977846174064955149290862569321978468622482839722413756570560574902614079729686524145351004748216637048440319989000889524345065854122758866688116427171479924442928230863465674813919123162824586178664583591245665294765456828489128831426076900422421902267105562632111110937054421750694165896040807198403850962455444362981230987879927244284909188845801561660979191338754992005240636899125607176060588611646710940507754100225698315520005593572972571636269561882670428252483600823257530420752963450)
+    -- putStrLn $ "Problem 9: " ++ show (problem9 1000)
+    -- putStrLn $ "Problem 10: " ++ show (problem10 2000000)
 
     print "--- Finished. ---"
