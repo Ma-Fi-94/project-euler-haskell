d3a5e0e6ffa6956b7138e32742706cf49fd5b6fe
Matthias M. Fischer <33027321+Ma-Fi-94@users.noreply.github.com>
2023-01-18 23:27:35 +0100

    Two more

--- euler0021-0030.hs
+++ euler0021-0030.hs
@@ -1,7 +1,9 @@
 -- Third part of the solutions to Project Euler (projecteuler.net).
 
-import Debug.Trace
 import qualified Data.Set as Set
+import Data.List (sort, permutations)
+import Data.List.Split (splitOn)
+import Data.Char (ord)
 
 -- For every a, there is always exactly one candidate b, which makes
 -- this run sufficiently fast.
@@ -13,15 +15,26 @@ problem21 n = sum [a+b | a <- [1..(n-1)],
   where
     d i = sum . filter (\x -> i `rem` x == 0) $ enumFromTo 1 (i-1)
 
+-- Love it.
+problem22 :: String -> Int
+problem22 = sum . zipWith (*) [1..] . map value . sort . map (tail . init) . splitOn ","
+  where
+    value = sum . map (subtract 64 . ord)
+    
 -- This is annoyingly slow (slightly below 2 min on my machine). 
--- problem23 :: Int -> Int
+problem23 :: Int -> Int
 problem23 i = sum . Set.elems . Set.difference allNbs $ remNbs
   where
     allNbs = Set.fromList [1..i]
     remNbs = Set.fromList [(x+y)| x <- abuNbs, y <- abuNbs]
     abuNbs = [x | x <- [1..i], d(x) > x]
     d i    = sum . filter (\x -> i `rem` x == 0) $ enumFromTo 1 (i `div` 2)
-    
+
+-- Bruteforce. We could also calculate this exactly, but the current approach
+-- does the trick sufficiently fast (few seconds on my machine).
+problem24 :: Int -> Int
+problem24 i = read $ (sort (permutations "0123456789")) !! (i-1)
+
 -- The famous Fibonacci memoisation strikes again, making this
 -- extremely efficient (runs in 200ms on my machine, I am
 -- legit surprised.)
@@ -32,8 +45,11 @@ problem25 d = length . takeWhile (<10^(d-1)) $ fibs
     
 main :: IO ()
 main = do
-    --putStrLn $ "Problem 21: " ++ show (problem21 10000)
-    putStrLn $ "Problem 23: " ++ show (problem23 28123)
-    putStrLn $ "Problem 25: " ++ show (problem25 1000)
+    -- putStrLn $ "Problem 21: " ++ show (problem21 10000)
+    -- p022_names <- readFile "p022_names.txt"
+    -- putStrLn $ "Problem 22: " ++ show (problem22 p022_names)
+    -- putStrLn $ "Problem 23: " ++ show (problem23 28123)
+    -- putStrLn $ "Problem 24: " ++ show (problem24 1000000)
+    -- putStrLn $ "Problem 25: " ++ show (problem25 1000)
     print "--- Finished. ---"
 
